14c4f85bfe8527d9c1ecd9f08fec9c7a748ad081
Darwin Gualotuña <daosgc@gmail.com>
2018-01-05 23:41:55 -0500

    add Typography

---
+++ src/components/atoms/Typography/Text.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './Typography.scss'
+
+import classNames from 'classnames/bind'
+const cx = classNames.bind(styles)
+
+const Text = ({highlight, secondary, disabled, inverted, small, lead, inline, bolder, lighter, error, className, style, children}) => {
+  const theProps = {
+    className: cx(className, styles.p, {
+      highlight,
+      secondary,
+      disabled,
+      inverted,
+      smallText: small,
+      lead,
+      lighter,
+      bolder,
+      error
+    }),
+    style
+  }
+
+  const tagElement = inline ? 'span' : 'p'
+  return React.createElement(tagElement, theProps, children)
+}
+
+Text.propTypes = {
+  children: PropTypes.any,
+  className: PropTypes.string,
+  highlight: PropTypes.bool,
+  secondary: PropTypes.bool,
+  disabled: PropTypes.bool,
+  inverted: PropTypes.bool,
+  style: PropTypes.object,
+  lead: PropTypes.bool,
+  small: PropTypes.bool,
+  inline: PropTypes.bool,
+  bolder: PropTypes.bool,
+  lighter: PropTypes.bool,
+  error: PropTypes.bool
+}
+export default Text
---
+++ src/components/atoms/Typography/index.js
@@ -0,0 +1,8 @@
+import Title from './Title'
+import Text from './Text'
+import Header from './Header'
+import Subtitle from './Subtitle'
+import Label from './Label'
+export default {
+  Title, Text, Header, Subtitle, Label
+}
