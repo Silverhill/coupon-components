1a40f12edba22f81d7d58185e0f13f4a4457f093
Darwin Gualotuña <daosgc@gmail.com>
2018-01-06 00:33:15 -0500

    add preloaders

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export {default as Button} from './components/atoms/Button/Button';
 export {default as Icon} from './components/atoms/Icon/Icon';
 export {default as Typography} from './components/atoms/Typography';
 export {default as InputBox} from './components/atoms/InputBox/InputBox';
+export {default as Preloader} from './components/atoms/Loaders';
