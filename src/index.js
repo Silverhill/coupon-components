980479885731a055fa57408c4ce634c8ed64a495
Darwin Gualotuña <daosgc@gmail.com>
2018-01-06 00:09:06 -0500

    add inputs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as Button} from './components/atoms/Button/Button';
 export {default as Icon} from './components/atoms/Icon/Icon';
 export {default as Typography} from './components/atoms/Typography';
+export {default as InputBox} from './components/atoms/InputBox/InputBox';
