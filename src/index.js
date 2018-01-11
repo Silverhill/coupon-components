cca2222014703fd648333d7e9218397f65ab5928
Darwin Gualotuña <daosgc@gmail.com>
2018-01-10 21:56:37 -0500

    add card

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ export {default as Icon} from './components/atoms/Icon/Icon';
 export {default as Typography} from './components/atoms/Typography';
 export {default as InputBox} from './components/atoms/InputBox/InputBox';
 export {default as Preloader} from './components/atoms/Loaders';
+export {default as Card} from './components/atoms/Card';
 export {default as Menu} from './components/molecules/Menu';
 export {default as Modal} from './components/molecules/Modal';
