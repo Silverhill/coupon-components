ee536870f9f1a812df37d2146df0069bbbae5167
Darwin Gualotuña <daosgc@gmail.com>
2018-01-10 21:25:03 -0500

    add menu

---
+++ src/components/molecules/Menu/MenuOptions.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Icon from 'Atoms/Icon'
+import Typography from 'Atoms/Typography'
+import styles from './Menu.scss'
+
+import classNames from 'classnames/bind'
+const cx = classNames.bind(styles)
+
+export default class MenuOptions extends Component {
+  componentDidMount () {
+    document.addEventListener('click', this.handleOutsideClick)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('click', this.handleOutsideClick)
+  }
+
+  handleOutsideClick = e => {
+    const { dismiss } = this.props
+    if (this.node && this.node.contains(e.target)) return
+    if (dismiss) dismiss()
+  }
+
+  render () {
+    const { options, leftMenu, onChange } = this.props
+    return (
+      <div className={cx(styles.list, { leftMenu })}>
+        {options && options.map((option, i) => {
+          const name = 'iconName' in option && option.iconName
+
+          return (
+            <div key={`option-${i}`} onClick={(e) => onChange(e, option)} className={styles.item}>
+              <Icon name={name} size={15} className={styles.itemIcon} />
+              <Typography.Text small className={styles.option}>{option.label}</Typography.Text>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+MenuOptions.propTypes = {
+  options: PropTypes.array,
+  leftMenu: PropTypes.bool,
+  onChange: PropTypes.func,
+  dismiss: PropTypes.func
+}
