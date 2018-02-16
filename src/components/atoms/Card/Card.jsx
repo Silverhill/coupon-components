c6673ba19b3774a268721bc618e55bd2cb3eaec1
Lenin Capa <leviskp87@gmail.com>
2018-02-16 18:59:25 -0500

    improvements

--- src/components/atoms/Card/Card.jsx
+++ src/components/atoms/Card/Card.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames/bind'
 import styles from './Card.css'
 import Text from '../Typography/Text'
 
-const cx = classNames.bind(styles);
+const cx = classNames.bind(styles)
 
 class Card extends Component {
   render () {
@@ -14,7 +14,7 @@ class Card extends Component {
         <div className={styles.description}>
           {title &&
             <div className={styles.title}>
-              <div class={styles.line}></div>
+              <div className={styles.line}></div>
               <Text small>
                 {title}
               </Text>
@@ -43,8 +43,8 @@ Card.propTypes = {
   flex: PropTypes.number,
   height: PropTypes.string,
   style: PropTypes.object,
-  title: PropTypes.object,
-  subtitle: PropTypes.object
+  title: PropTypes.string,
+  subtitle: PropTypes.string
 }
 
 export default Card;
