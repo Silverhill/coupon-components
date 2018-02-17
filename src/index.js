d6dbcb0ec2072733314e87efe636d07838a26aa4
Lenin Capa <leviskp87@gmail.com>
2018-02-17 12:10:11 -0500

    add index component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export {default as InputBox} from './components/atoms/InputBox/InputBox';
 export {default as InputNested} from './components/atoms/InputNested/InputNested';
 export {default as Preloader} from './components/atoms/Loaders';
 export {default as Card} from './components/atoms/Card';
-export {default as Avatar} from './components/atoms/Avatar/Avatar';
+export {default as Avatar} from './components/atoms/Avatar';
 export {default as Menu} from './components/molecules/Menu';
 export {default as Modal} from './components/molecules/Modal';
 export {default as HeaderBar} from './components/molecules/HeaderBar';
